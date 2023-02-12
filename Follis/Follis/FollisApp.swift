d28fa238e19a2c143ef3fd0a3ed34e17c35f0d98
AkhilMah14 <akhil@maheeps.net>
2023-02-12 18:37:17 -0500

    Initalizing repo

---
+++ Follis/Follis/FollisApp.swift
@@ -0,0 +1,17 @@
+//
+//  FollisApp.swift
+//  Follis
+//
+//  Created by Akhil Maheepathi on 2/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FollisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
